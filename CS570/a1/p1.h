b0724667366837610596e5eb99959b3b5c977051
ductiletoaster <brian@th3collective.com>
2013-05-28 20:22:59 -0700

    Adding basic new template files

---
+++ CS570/a1/p1.h
@@ -0,0 +1,17 @@
+/** 
+ * @author: Brian Gebel
+ * @version: 05.28.2013
+ * @description: SDSU Summer 2013 CS570 Program 1
+ *
+ */
+
+// Program system includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// Included guard definition 
+#ifndef P1_H_INCLUDED
+#define P1_H_INCLUDED
+
+
+#endif
---
+++ CS570/a1/p1main.c
@@ -0,0 +1,17 @@
+/** 
+ * @author: Brian Gebel
+ * @version: 05.28.2013
+ * @description: SDSU Summer 2013 CS570 Program 1
+ *
+ */
+
+// Program includes
+#include "p1.h"
+
+/**
+ * Intializes Program one
+ */
+int main() {
+	printf("%s\n", "Hello World");
+	exit(0);
+}
